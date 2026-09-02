94aa5d7ad74c5c16c99fa5b3db3c7612b6dd3f51
KHOEM_AI <khoemaikhoemai@gmail.com>
2026-09-02 08:01:03 +0700

    ID_AI_369_400_401

---
+++ khoem-now/src/components/CAISection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function CAISection() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>CAI</h1>
+
+      {/* ដាក់ Interface និង UI របស់ CAI នៅទីនេះ */}
+      <p>ផ្នែក CAI នឹងបង្ហាញនៅទីនេះ។</p>
+    </main>
+  );
+}
